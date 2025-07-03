fe0c491174b9961a3bbd4db02b43f649adf3d626
Annie Johansson <162436676+ann1ejohansson@users.noreply.github.com>
2025-07-03 10:16:12 +0200

    Update index.js

--- site/components/index.js
+++ site/components/index.js
@@ -1,4 +1,4 @@
-import HeaderComponent from 'header/header.js';
+import HeaderComponent from 'site/components/header/header.js';
 import FooterComponent from 'footer/footer.js';
 import ContentBlockComponent from './contentBlock/contentBlock.js'; 
 customElements.define('header-component', HeaderComponent);
