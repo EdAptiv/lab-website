62676e70ac0661a517f73e23c203c202da0052c2
Annie Johansson <162436676+ann1ejohansson@users.noreply.github.com>
2025-07-03 11:00:19 +0200

    update path

--- site/components/index.js
+++ site/components/index.js
@@ -1,4 +1,4 @@
-import HeaderComponent from './components/header/header.js';
-import FooterComponent from './components/footer/footer.js';
+import HeaderComponent from './header/header.js';
+import FooterComponent from './footer/footer.js';
 customElements.define('header-component', HeaderComponent);
 customElements.define('footer-component', FooterComponent);
